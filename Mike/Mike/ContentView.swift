e988c6e3b0e1d42c9be475f636ec9a2d44c9ecd6
殷聃 <1103720832@qq.com>
2021-04-27 19:03:58 +0800

    Revert "move Mike dir"
    This reverts commit 5f6580bc2006d267df946b90c7d40a7f4ea1133a.

---
+++ Mike/Mike/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mike
+//
+//  Created by Fanghao Yang on 4/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
