b86d4b5930c8a307654a80d30f42dd23bb52596d
kaisuki <1103720832@qq.com>
2021-06-28 17:44:21 +0800

    MK-146 #comment new method to adapt window rotation for video and camera view

--- Mike/Functions/User/UserContentDetailViewController.swift
+++ Mike/Functions/User/UserContentDetailViewController.swift
@@ -234,7 +234,11 @@ extension UserContentDetailViewController:UITableViewDelegate,UITableViewDataSou
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.section == 0 {
             DispatchQueue.main.async {
-                let vc:VideoViewController = VideoViewController()
+//                let vc:VideoViewController = VideoViewController()
+//                vc.videoModel = self.userContentModel
+//                vc.modalPresentationStyle = .fullScreen
+//                self.present(vc, animated: true, completion: nil)
+                let vc:SplitVideoViewController = SplitVideoViewController()
                 vc.videoModel = self.userContentModel
                 vc.modalPresentationStyle = .fullScreen
                 self.present(vc, animated: true, completion: nil)
